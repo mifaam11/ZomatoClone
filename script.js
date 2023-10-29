d9c7074f2eb0df030f06ad64c487098ac81d1acf
ifaam <mifaam11@gmail.com>
2023-10-30 00:37:24 +0530

    section_main

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
-let main_div = document.createElement("div");
-main_div.setAttribute("class","main_section");
 let body = document.getElementsByTagName("body")[0];
-body.insertAdjacentElement("afterbegin", main_div);
 body.style.margin = 0;
 body.style.padding = 0;
+// =========================      Top Container         =============================
+let top_container = document.createElement("div");
+top_container.setAttribute("class","top-container");
+body.insertAdjacentElement("afterbegin", top_container);
+top_container.style.background = "url(img/home.png)";
+top_container.style.backgroundRepeat = "no-repeat";
+top_container.style.minHeight = "25rem";
+top_container.style.maxHeight = "30rem";
+top_container.style.width = "100%";
+top_container.style.overflow = "hidden";
+top_container.style.backgroundSize = "cover";
+top_container.style.backgroundPosition = "center";
+
+// ================          header                 ============================
+let menu_header = document.createElement("header");
+top_container.insertAdjacentElement("afterbegin", menu_header);
+menu_header.style.padding = "7px 0px 7px 0px";
+menu_header.style.height = "100%";
+menu_header.style.width = "100%";
+menu_header.style.display = "flex";
+menu_header.style.justifyContent = "space-around";
 
 
-let img_div = document.createElement("div");
-img_div.setAttribute("class", "bg_home")
-main_div.insertAdjacentElement("afterbegin", img_div);
-let homeImg = document.createElement("img");
-img_div.insertAdjacentElement("afterbegin",homeImg)
-homeImg.setAttribute("src", "img/home.png");
+let empDiv = document.createElement("div");
+menu_header.insertAdjacentElement("afterbegin", empDiv)
+let nav_bar = document.createElement("nav");
+menu_header.insertAdjacentElement("beforeend", nav_bar);
+nav_bar.style.display = "flex";
+
+
+let menu_ul = document.createElement("ul");
+menu_ul.setAttribute("class", "menu_ul");
+nav_bar.insertAdjacentElement("afterbegin", menu_ul);
+menu_ul.style.listStyleType = "none"
+menu_ul.style.display = "flex";
+menu_ul.style.justifyContent = "space-between";
+menu_ul.style.gap = "2rem";
 
 
 
-let nav_div = document.createElement("div");
-nav_div.setAttribute("class", "menu");
-img_div.insertAdjacentElement("beforeend", nav_div);
-let menu_ul = document.createElement("ul")
-menu_ul.setAttribute("class", "menu_ul")
-nav_div.insertAdjacentElement("afterbegin", menu_ul);
-// let menu_item = document.createElement("li")
-
+// =========================        menu item                       ====================
 let menuItems = ["Add restaurent", "Log in", "sign up"]
 for (let i=0; i<menuItems.length; i++){
     let menu_li=document.createElement('li');
     menu_ul.appendChild(menu_li);
     menu_li.innerHTML=menu_li.innerHTML + menuItems[i];
-    console.log(menu_li);
+    menu_li.style.listStyle= "none";
+    menu_li.style.color= "white";
+    menu_li.style.fontSize= "1.4em";
 }
+
+// =============              Head line / home section ==================
+
+let main_section = document.createElement("section");
+top_container.insertAdjacentElement("beforeend",main_section)
+
